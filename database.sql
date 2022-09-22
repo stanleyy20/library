3c190b00a1f43507ab3257ad86cc16e042f379f4
Dawid Stach <dstach20@gmail.com>
2022-09-22 12:50:29 +0200

    add new values

--- database.sql
+++ database.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE library
-
 CREATE TABLE books(
 book_id SERIAL,
 title VARCHAR(50) NOT NULL,
@@ -12,15 +10,24 @@ UNIQUE (isbn)
 
 SELECT * FROM books;
 
-INSERT INTO books (title, author, isbn, available) VALUES ('Rethinking Productivity in Software Engineering','Caitlin Sadowski and Thomas Zimmermann','9781484242216', 'true');
+INSERT INTO books (title, author, isbn, available) VALUES ('Eloquent JavaScript Third Edition','Marijn Haverbeke','9781593279509', 'true'),
+('Practical Modern JavaScript','Nicolás Bevacqua','9781491943533', 'true'),
+('Understanding ECMAScript 6','Nicholas C. Zakas','9781593277574', 'true'),
+('Speaking JavaScript','Axel Rauschmayer','9781449365035', 'true'),
+('Learning JavaScript Design Patterns','Addy Osmani','9781449331818', 'true'),
+('You Dont Know JS Yet','Kyle Simpson','9798602477429', 'true'),
+('Pro Git','Scott Chacon and Ben Straub','9781484200766', 'true'),
+('Rethinking Productivity in Software Engineering','Caitlin Sadowski and Thomas Zimmermann','9781412384242216', 'true');
+
 
 CREATE TABLE users (
-user_id serial,
+user_id SERIAL,
 name VARCHAR(25) NOT NULL,
 email VARCHAR(25) NOT NULL,
 password VARCHAR(250) NOT NULL,
 role VARCHAR(25) NOT NULL, 
-PRIMARY KEY (user_id)
+PRIMARY KEY (user_id),
+UNIQUE (email)
 );
 
 
@@ -32,7 +39,4 @@ PRIMARY KEY(id),
 FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE,
 FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
---psql -U postgres
---\c library
---\dt
---heroku pg:psql
+
